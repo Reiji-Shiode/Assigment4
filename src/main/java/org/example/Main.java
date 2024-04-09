39ce9a626c77d4777c826243c1540bdcf5fe07c1
Reiji-Shiode <rerere0606@gmail.com>
2024-04-09 23:35:33 +0900

    ç第â4回課題ãStream[r]ã、gitignoreを変更

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,11 @@
 package org.example;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> names = List.of("reina", "ren", "reo", "karen", "renald", "andy");
+        List<String> result = names.stream().filter(name -> name.startsWith("r")).toList();
+        System.out.println(result);
     }
 }
